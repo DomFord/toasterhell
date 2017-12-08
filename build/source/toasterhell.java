66059e04486d040ccf07e9282b4d9ffb33255d75
DomFord <dominic.ford@live.com>
2017-12-08 12:45:10 +0100

    added hit blinker

--- build/source/toasterhell.java
+++ build/source/toasterhell.java
@@ -432,7 +432,7 @@ This script handles the player, both what player is selected, player life, weapo
 */
 class PlayerManager{
   int timeStamp, shootRateModifier, playerSelect, avatarFrame, ticksLast, frameDuration, score, health;
-  float xpos, ypos, maxSpeed, size, leftSpeed, rightSpeed, upSpeed, downSpeed, speedModifier, brakeModifier;
+  float xpos, ypos, maxSpeed, size, leftSpeed, rightSpeed, upSpeed, downSpeed, speedModifier, brakeModifier, hitBlinkOpacity;
   boolean alive, left, right, up, down, shooting;
   ArrayList<PlayerBullet> bullets;
   PImage player1sheet;
@@ -456,6 +456,7 @@ class PlayerManager{
     rightSpeed = constrain(rightSpeed, 0, maxSpeed);
     upSpeed = constrain(upSpeed, 0, maxSpeed);
     downSpeed = constrain(downSpeed, 0, maxSpeed);
+    hitBlinkOpacity = 0;
     speedModifier = 0.2f;
     brakeModifier = 0.5f;
     bullets = new ArrayList<PlayerBullet>();
@@ -475,6 +476,7 @@ class PlayerManager{
       speedHandler();
       movePlayer();
       shootHandler();
+
       imageMode(CENTER);
       if(playerSelect == 1){
         PImage f = player1sheet.get((avatarFrame*60),0,60,66);
@@ -505,6 +507,7 @@ class PlayerManager{
       rect(xpos, ypos, size, size);
       movePlayer();
       shoot();*/
+
       } else {
         death();
       }
@@ -514,6 +517,7 @@ class PlayerManager{
       speedHandler();
       shootHandler();
       bulletCollision();
+      hitBlinker();
       displayLife();
       if (xpos - size < 10) {
         leftSpeed = 0;
@@ -597,6 +601,7 @@ class PlayerManager{
             && enemyManager.basicEnemies.get(i).bullets.get(j).ypos + enemyManager.basicEnemies.get(i).bullets.get(j).size / 2 < ypos + size / 2) {
               println("Player hit!");
               enemyManager.basicEnemies.get(i).bullets.remove(j);
+              hitBlinkOpacity = 255 / 2;
               health--;
               println(health);
             }
@@ -604,22 +609,30 @@ class PlayerManager{
       }
   }
 
+  public void hitBlinker() {
+    fill(218, 44, 56, hitBlinkOpacity);
+    rectMode(CENTER);
+    rect(width / 2, height / 2, width, height);
+    if (hitBlinkOpacity > 0) {
+      hitBlinkOpacity -= 5;
+    }
+  }
+
   public void displayLife() {
     for (int i = 0; i <= health - 1; i++) {
       if (health >= 3) {
         if (millis() / 100 % 10 != 0) {
-          image(heart, width - 50 - (i * 35), height - 50);
+          image(heart, width - 40 - (i * 35), height - 40);
         }
       } else if (health == 2) {
         if (millis() / 100 % 5 != 0) {
-          image(heart, width - 50 - (i * 35), height - 50);
+          image(heart, width - 40 - (i * 35), height - 40);
         }
       } else {
         if (millis() / 100 % 2 != 0) {
-          image(heart, width - 50 - (i * 35), height - 50);
+          image(heart, width - 40 - (i * 35), height - 40);
         }
       }
-
     }
   }
 
